031b0940cf7843b212be8a67e1744b3b2a6be82d
jonahf <jonahf@sxi.com.ph>
2021-08-26 20:38:53 +0800

    change user to 1000

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,13 @@
-#FROM alpine:latest
 FROM ubuntu:18.04
-#RUN apk add expect && expect -v
 RUN apt update && apt install expect -y \
     && apt-get install -y --no-install-recommends firefox \
     && rm -rf /var/lib/apt/lists/*
 COPY . /sxi/
 RUN chmod +x /sxi/docker-entrypoint.sh && cp /sxi/docker-entrypoint.sh /opt/docker-entrypoint.sh \
-  && chmod +x /sxi/tp-expect.sh && ./sxi/tp-expect.sh && rm -rf /sxi 
-  #&& chown 989:989 -R /opt/ 
+  && chmod +x /sxi/tp-expect.sh && ./sxi/tp-expect.sh && rm -rf /sxi \ 
+  && chown 1000:1000 -R /opt/ 
 ENV PATH="/opt/testproject/agent/bin/:${PATH}"
-#USER 989
+USER 1000
 ENV HOME=/tmp
 WORKDIR /tmp
 ENTRYPOINT ["/opt/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,30 +1,37 @@
 #!/bin/sh
 set -e
+echo "Test params csv: "$DATASOURCE
 
 if [ "$1" = "debug" ]; then
    bash
 elif [ "$1" = "testzip" ] && [ -d "$1" ]; then
    echo "testzip exists"
-   #mkdir -p /tmp/?/.testproject/agent || echo "already exists"   
-   mkdir -p /root/.testproject/agent || echo "already exists"
+   mkdir -p /tmp/?/.testproject/agent || echo "already exists"   
+   #(if root)mkdir -p /root/.testproject/agent || echo "already exists"
    testproject-agent start -fs -d /tmp
    echo "wait 20s to start" && sleep 20s    
-   #cp /tmp/runtime.json /tmp/?/.testproject/agent/ || echo "nothing to copy"    
-   cp /tmp/runtime.json /root/.testproject/agent || echo "nothing to copy"    
+   cp /tmp/runtime.json /tmp/?/.testproject/agent/ || echo "nothing to copy"    
+   #(if root)cp /tmp/runtime.json /root/.testproject/agent || echo "nothing to copy"    
    testproject-agent browsers
-   mkdir -p /tmp/reports    
-   for file in testzip/*; do testproject-agent run $file -o /tmp/reports; done
+   mkdir -p /tmp/reports
+   if [ -z ${DATASOURCE+x} ]; 
+     then for file in testzip/*; do testproject-agent run $file -o /tmp/reports; done; 
+   else for file in testzip/*; do testproject-agent run $file -o /tmp/reports -p $DATASOURCE; done; 
+   fi    
 else
    mkdir -p /tmp/?/.testproject/agent || echo "already exists"
-   #mkdir -p /root/.testproject/agent || echo "already exists"
+   #(if root)mkdir -p /root/.testproject/agent || echo "already exists"
    testproject-agent start -fs -d /tmp
-   echo "wait 20s to start" && sleep 20s
+   echo "wait 20s to start" && sleep 20ss
    cp /tmp/runtime.json /tmp/?/.testproject/agent/ || echo "nothing to copy"
    #cp /tmp/runtime.json /root/.testproject/agent || echo "nothing to copy"
    testproject-agent browsers
    echo "$1"
    mkdir -p /tmp/reports 
-   testproject-agent run "$1" -o /tmp/reports
+   if [ -z ${DATASOURCE+x} ]; 
+     then testproject-agent run "$1" -o /tmp/reports; 
+   else testproject-agent run "$1" -o /tmp/reports -p $DATASOURCE; 
+   fi  
 fi
 
 
